b012a05b77a39c9a5d449cb0a98ad5f26115386e
joseph alemu <joseph.alemu01@gmail.com>
2018-02-19 14:14:16 -0500

    first commit

---
+++ app/src/main/java/geocoding/joseph/com/geocoding/MyCustomAlertDialog.java
@@ -0,0 +1,44 @@
+package geocoding.joseph.com.geocoding;
+
+import android.app.Dialog;
+import android.content.Context;
+
+/**
+ * Created by Admin on 2/19/2018.
+ */
+
+
+//public class MyCustomAlertDialog {
+//
+//
+//    Context context;
+//    public MyCustomAlertDialog(Context context) {
+//        this.context = context;
+//    }
+//
+//       Dialog MyDialog = new Dialog(MainActivity.this);
+//        MyDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        MyDialog.setContentView(R.layout.customdialog);
+//        MyDialog.setTitle("My Custom Dialog");
+//
+//      hello = (Button)MyDialog.findViewById(R.id.hello);
+//      close = (Button)MyDialog.findViewById(R.id.close);
+//
+//        hello.setEnabled(true);
+//        close.setEnabled(true);
+//
+//        hello.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            Toast.makeText(getApplicationContext(), "Hello, I'm Custom Alert Dialog", Toast.LENGTH_LONG).show();
+//        }
+//    });
+//        close.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            MyDialog.cancel();
+//        }
+//    });
+//
+//        MyDialog.show();
+//}
---
+++ app/src/main/java/geocoding/joseph/com/geocoding/PlacesAutoCompleteActivity.java
@@ -0,0 +1,290 @@
+package geocoding.joseph.com.geocoding;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.PlacePhotoMetadata;
+import com.google.android.gms.location.places.PlacePhotoMetadataBuffer;
+import com.google.android.gms.location.places.PlacePhotoMetadataResponse;
+import com.google.android.gms.location.places.PlacePhotoResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.android.gms.location.places.GeoDataClient;
+import geocoding.joseph.com.geocoding.adapters.PlacesAutoCompleteAdapter;
+import geocoding.joseph.com.geocoding.listeners.RecyclerItemClickListener;
+import geocoding.joseph.com.geocoding.utility.Constants;
+
+
+public class PlacesAutoCompleteActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+    protected GoogleApiClient mGoogleApiClient;
+
+    private static final LatLngBounds BOUNDS_INDIA = new LatLngBounds(
+            new LatLng(-0, 0), new LatLng(0, 0));
+
+    private EditText mAutocompleteView;
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
+    private PlacesAutoCompleteAdapter mAutoCompleteAdapter;
+    ImageView delete;
+    String StrAttribution;
+
+    private GeoDataClient mGeoDataClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        buildGoogleApiClient();
+        setContentView(R.layout.activity_search);
+
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+        mAutocompleteView = (EditText)findViewById(R.id.autocomplete_places);
+
+        delete=(ImageView)findViewById(R.id.cross);
+
+        mAutoCompleteAdapter =  new PlacesAutoCompleteAdapter(this, R.layout.searchview_adapter,
+                mGoogleApiClient, BOUNDS_INDIA, null);
+
+        mRecyclerView=(RecyclerView)findViewById(R.id.recyclerView);
+        mLinearLayoutManager=new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        mRecyclerView.setAdapter(mAutoCompleteAdapter);
+        delete.setOnClickListener(this);
+        mAutocompleteView.addTextChangedListener(new TextWatcher() {
+
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                if (!s.toString().equals("") && mGoogleApiClient.isConnected()) {
+                    mAutoCompleteAdapter.getFilter().filter(s.toString());
+                }else if(!mGoogleApiClient.isConnected()){
+                    Toast.makeText(getApplicationContext(), Constants.API_NOT_CONNECTED,Toast.LENGTH_SHORT).show();
+                    Log.e(Constants.PlacesTag, Constants.API_NOT_CONNECTED);
+                }
+
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+
+            }
+
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mRecyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(this, new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        final PlacesAutoCompleteAdapter.PlaceAutocomplete item = mAutoCompleteAdapter.getItem(position);
+                        final String placeId = String.valueOf(item.placeId);
+                        Log.i("TAG", "Autocomplete item selected: " + item.description);
+                        /*
+                             Issue a request to the Places Geo Data API to retrieve a Place object with additional details about the place.
+                         */
+
+                        PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                                .getPlaceById(mGoogleApiClient, placeId);
+                        placeResult.setResultCallback(new ResultCallback<PlaceBuffer>() {
+                            @Override
+                            public void onResult(PlaceBuffer places) {
+                                if(places.getCount()==1){
+
+
+                                    //Do the things here on Click.....
+                                    Toast.makeText(getApplicationContext(),String.valueOf(places.get(0).getLatLng()),Toast.LENGTH_SHORT).show();
+                                }else {
+                                    Toast.makeText(getApplicationContext(),Constants.SOMETHING_WENT_WRONG,Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                        StrAttribution = item.toString();
+                        Log.i("TAG", "Clicked: " + item.description);
+                        Log.i("TAG", "Called getPlaceById to get Place details for " + item.placeId);
+                        getPhotos(item.placeId.toString());
+                    }
+                })
+        );
+    }
+
+    // Request photos and metadata for the specified place.
+    private void getPhotos(String placeId) {
+        //final String placeId = "ChIJa147K9HX3IAR-lwiGIQv9i4";
+        final Task<PlacePhotoMetadataResponse> photoMetadataResponse = mGeoDataClient.getPlacePhotos(placeId);
+        photoMetadataResponse.addOnCompleteListener(new OnCompleteListener<PlacePhotoMetadataResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<PlacePhotoMetadataResponse> task) {
+                // Get the list of photos.
+                PlacePhotoMetadataResponse photos = task.getResult();
+                // Get the PlacePhotoMetadataBuffer (metadata for all of the photos).
+                PlacePhotoMetadataBuffer photoMetadataBuffer = photos.getPhotoMetadata();
+                // Get the first photo in the list.
+                PlacePhotoMetadata photoMetadata = photoMetadataBuffer.get(0);
+                // Get the attribution text.
+                CharSequence attribution = photoMetadata.getAttributions();
+
+                // Get a full-size bitmap for the photo.
+                Task<PlacePhotoResponse> photoResponse = mGeoDataClient.getPhoto(photoMetadata);
+                photoResponse.addOnCompleteListener(new OnCompleteListener<PlacePhotoResponse>() {
+                    @Override
+                    public void onComplete(@NonNull Task<PlacePhotoResponse> task) {
+                        PlacePhotoResponse photo = task.getResult();
+                        Bitmap bitmap = photo.getBitmap();
+                        startAuthDialog(bitmap,StrAttribution);
+                    }
+                });
+            }
+        });
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .addApi(Places.GEO_DATA_API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.v("Google API Callback", "Connection Done");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.v("Google API Callback", "Connection Suspended");
+        Log.v("Code", String.valueOf(i));
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.v("Google API Callback","Connection Failed");
+        Log.v("Error Code", String.valueOf(connectionResult.getErrorCode()));
+        Toast.makeText(this, Constants.API_NOT_CONNECTED,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==delete){
+            mAutocompleteView.setText("");
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!mGoogleApiClient.isConnected() && !mGoogleApiClient.isConnecting()){
+            Log.v("Google API","Connecting");
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(mGoogleApiClient.isConnected()){
+            Log.v("Google API","Dis-Connecting");
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+
+    private void startAuthDialog( Bitmap bitmap, String attribution) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        alertDialog.getWindow().setLayout(500, 500);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.customdialog, null);
+        alertDialog.getWindow().setContentView(dialogView);
+        ImageView imageView = (ImageView) dialogView.findViewById(R.id.ivDialog);
+        TextView textView = (TextView) dialogView.findViewById(R.id.tvDescription);
+        textView.setText(attribution);
+        imageView.setImageBitmap(bitmap);
+
+
+        Button btnOpenmap = (Button)dialogView.findViewById(R.id.btnOpenMap);
+        Button btnClose = (Button)dialogView.findViewById(R.id.close);
+
+        btnOpenmap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(PlacesAutoCompleteActivity.this,GoogleMapsActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnOpenmap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+    }
+
+
+}
